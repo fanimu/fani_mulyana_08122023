c3bd348dc3bb5fb20d6508bcfcbeef8b4184147a
fanimu <fanimu7@gmail.com>
2023-12-08 11:28:41 +0700

    perbaikan tampilan create dan edit

--- pegawai/resources/views/pegawai/create.blade.php
+++ pegawai/resources/views/pegawai/create.blade.php
@@ -4,25 +4,34 @@
     <title>Tambah Pegawai</title>
 </head>
 <body>
-    <h1>Tambah Pegawai</h1>
+<a href="{{ route('pegawai.index') }}">Kembali</a>
 
+    <h1>Tambah Pegawai</h1>
     <form action="{{ route('pegawai.store') }}" method="post" enctype="multipart/form-data">
         @csrf
-        <label for="pegawai_nama">Nama:</label>
-        <input type="text" name="pegawai_nama" required><br>
-
-        <label for="pegawai_umur">Umur:</label>
-        <input type="number" name="pegawai_umur" required><br>
-
-        <label for="pegawai_alamat">Alamat:</label>
-        <input type="text" name="pegawai_alamat" required><br>
-
-        <label for="foto">Foto:</label>
-        <input type="file" name="foto"><br>
-
-        <button type="submit">Simpan</button>
+        <table>
+            <tr>
+                <td><label for="pegawai_nama">Nama:</label></td>
+                <td><input type="text" name="pegawai_nama" required><br></td>
+            </tr>
+            <tr>
+                <td><label for="pegawai_umur">Umur:</label></td>
+                <td><input type="number" name="pegawai_umur" required><br></td>
+            </tr>
+            <tr>
+                <td><label for="pegawai_alamat">Alamat:</label></td>
+                <td><input type="text" name="pegawai_alamat" required><br></td>
+            </tr>
+            <tr>
+                <td><label for="foto">Foto:</label></td>
+                <td><input type="file" name="foto"><br></td>
+            </tr>
+        </table>
+       
+            <br>
+            <button type="submit">Simpan</button>
+        
     </form>
 
-    <a href="{{ route('pegawai.index') }}">Kembali</a>
 </body>
 </html>
--- pegawai/resources/views/pegawai/edit.blade.php
+++ pegawai/resources/views/pegawai/edit.blade.php
@@ -4,33 +4,42 @@
     <title>Edit Pegawai</title>
 </head>
 <body>
+<a href="{{ route('pegawai.index') }}">Kembali</a>
+
     <h1>Edit Pegawai</h1>
 
     <form action="{{ route('pegawai.update', $pegawai->id) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         
-        <label for="pegawai_nama">Nama:</label>
-        <input type="text" name="pegawai_nama" value="{{ $pegawai->pegawai_nama }}" required><br>
-
-        <label for="pegawai_umur">Umur:</label>
-        <input type="number" name="pegawai_umur" value="{{ $pegawai->pegawai_umur }}" required><br>
-
-        <label for="pegawai_alamat">Alamat:</label>
-        <input type="text" name="pegawai_alamat" value="{{ $pegawai->pegawai_alamat }}" required><br>
-
-        <label for="foto">Foto:</label>
-        <input type="file" name="foto"><br>
-
-        @if($pegawai->foto)
+        <table>
+            <tr>
+                <td><label for="pegawai_nama">Nama:</label></td>
+                <td><input type="text" name="pegawai_nama" value="{{ $pegawai->pegawai_nama }}" required><br></td>
+            </tr>
+            <tr>
+                <td><label for="pegawai_umur">Umur:</label></td>
+                <td><label for="pegawai_umur">Umur:</label></td>
+            </tr>
+            <tr>
+                <td><label for="pegawai_alamat">Alamat:</label></td>
+                <td><input type="text" name="pegawai_alamat" value="{{ $pegawai->pegawai_alamat }}" required><br></td>
+            </tr>
+            <tr>
+                <td><label for="foto">Foto:</label></td>
+                <td><input type="file" name="foto"><br></td>
+            </tr>
+        </table>
+        <!-- @if($pegawai->foto)
             <img src="{{ asset($pegawai->foto) }}" alt="Foto Pegawai" width="100">
         @else
             Tidak Ada Foto
-        @endif
+        @endif -->
+
+        <br>
 
         <button type="submit">Simpan</button>
     </form>
 
-    <a href="{{ route('pegawai.index') }}">Kembali</a>
 </body>
 </html>

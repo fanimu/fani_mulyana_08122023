3bc238a46c95470af470e0dc4665db9569751441
fanimu <fanimu7@gmail.com>
2023-12-08 11:17:15 +0700

    perbaikan tampilan index

--- pegawai/resources/views/pegawai/index.blade.php
+++ pegawai/resources/views/pegawai/index.blade.php
@@ -2,13 +2,56 @@
 <html>
 <head>
     <title>Daftar Pegawai</title>
+    <style>
+        .btn_add{
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 8px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .btn_edit{
+           background-color: #008CBA;
+           border: none;
+           color: white;
+           padding: 8px 16px;
+           text-align: center;
+           text-decoration: none;
+           display: inline-block;
+           font-size: 16px;
+           margin: 4px 2px;
+           cursor: pointer;
+        }
+
+        .btn_delete{
+            background-color: #f44336;
+            border: none;
+           color: white;
+           padding: 8px 16px;
+           text-align: center;
+           text-decoration: none;
+           display: inline-block;
+           font-size: 16px;
+           margin: 4px 2px;
+           cursor: pointer;
+        }
+
+        table{
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <h1>Daftar Pegawai</h1>
 
-    <a href="{{ route('pegawai.create') }}">Tambah Pegawai</a>
+    <a class="btn_add" href="{{ route('pegawai.create') }}">Tambah Pegawai</a>
 
-    <table border="1">
+    <table border="1" width="100%">
         <tr>
             <th>Nama</th>
             <th>Umur</th>
@@ -29,12 +72,12 @@
                 @endif
             </td>
             <td>
-                <a href="{{ route('pegawai.show', $p->id) }}">Detail</a>
-                <a href="{{ route('pegawai.edit', $p->id) }}">Edit</a>
+                <!-- <a href="{{ route('pegawai.show', $p->id) }}">Detail</a> -->
+                <a class="btn_edit" href="{{ route('pegawai.edit', $p->id) }}">Edit</a>
                 <form action="{{ route('pegawai.destroy', $p->id) }}" method="post">
                     @csrf
                     @method('DELETE')
-                    <button type="submit">Hapus</button>
+                    <button class="btn_delete" type="submit">Hapus</button>
                 </form>
             </td>
         </tr>
